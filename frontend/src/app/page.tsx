2cd89a24e4163cd59b11c2ca501e7ac8802e9a66
thekavikumar <kavikumarceo@gmail.com>
2024-03-29 14:12:46 +0530

    home

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,29 @@
 import {
   RegisterLink,
   LoginLink,
+  LogoutLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
   return (
     <main className="h-screen flex items-center justify-center">
       <div className="flex items-center gap-6">
-        <LoginLink>Sign in</LoginLink>
-        <RegisterLink>Sign up</RegisterLink>
+        {!user && (
+          <>
+            <LoginLink>Sign in</LoginLink>
+            <RegisterLink>Sign up</RegisterLink>
+          </>
+        )}
+        {user && (
+          <div>
+            <h1>Welcome {user?.given_name}</h1>
+            <LogoutLink>Sign out</LogoutLink>
+          </div>
+        )}
       </div>
     </main>
   );
